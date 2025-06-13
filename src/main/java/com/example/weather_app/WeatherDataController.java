437ef6eb9a86def03f423966d222ea2dd78e1e04
Clemens Brunner <brunnerclemens58@gmail.com>
2025-06-13 10:16:10 +0200

    Added put and delete

--- src/main/java/com/example/weather_app/WeatherDataController.java
+++ src/main/java/com/example/weather_app/WeatherDataController.java
@@ -3,9 +3,7 @@ package com.example.weather_app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -23,4 +21,42 @@ public class WeatherDataController {
         }
         return new ResponseEntity("No data for city " + city + " found", HttpStatus.NOT_FOUND);
     }
+
+    @GetMapping("/weather/all")
+    public ResponseEntity<Iterable<WeatherData>> getAllWeatherData() {
+        var allWeatherData = weatherDataRepository.findAll();
+
+        return new ResponseEntity<Iterable<WeatherData>>(allWeatherData, HttpStatus.OK);
+    }
+
+    @PostMapping("/weather")
+    public ResponseEntity<WeatherData> saveWeatherData(@RequestBody WeatherData newWeatherData) {
+        weatherDataRepository.save(newWeatherData);
+        return new ResponseEntity<WeatherData>(newWeatherData, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/weather")
+    public ResponseEntity deleteWeatherData(@RequestParam(value = "city") String city) {
+        Optional<WeatherData> weatherDataToDelete= weatherDataRepository.findByCity(city);
+
+        if(weatherDataToDelete.isPresent()) {
+            weatherDataRepository.deleteById(weatherDataToDelete.get().getId());
+            return new ResponseEntity("Weather Data of " + weatherDataToDelete.get().getCity() + " was deleted.", HttpStatus.OK);
+        }
+        return new ResponseEntity("No Weather Data for following city found: " + weatherDataToDelete.get().getCity(), HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/weather")
+    public ResponseEntity<WeatherData> editWeatherData(@RequestBody WeatherData editedWeatherData) {
+        Optional<WeatherData> weatherDataInDb = weatherDataRepository.findByCity(editedWeatherData.getCity());
+
+        if(weatherDataInDb.isPresent()) {
+            editedWeatherData.setId(weatherDataInDb.get().getId());
+            WeatherData savedWeatherData = weatherDataRepository.save(editedWeatherData);
+            return new ResponseEntity<WeatherData>(savedWeatherData, HttpStatus.OK);
+        }
+        return new ResponseEntity("No existing city to update with name " + editedWeatherData.getCity() + " found", HttpStatus.NOT_FOUND);
+    }
+
+
 }
